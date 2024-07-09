f58ec41019bc894c53555f03eef2b02671bfcbb5
Jam <jamayub098@gmail.com>
2024-07-10 01:35:22 +0500

    event handeling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,14 +17,29 @@ class Counter extends Component {
       </ul>
     );
   }
+  handleIncrement() {
+    console.log("incremented!!");
+  }
   render() {
     return (
       <div>
-        {this.state.tag.length === 0 && "Please add a tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button onClick = {this.handleIncrement} className="btn btn-secondory btn-sm">Increment</button>
       </div>
     );
   }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
 }
 
 export default Counter;
