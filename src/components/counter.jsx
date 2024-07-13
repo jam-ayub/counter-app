36668b33f2d6633370da64a87e8e387bfdd53a1e
Jam <jamayub098@gmail.com>
2024-07-13 23:33:47 +0500

    passing childerns

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -28,6 +28,7 @@ class Counter extends Component {
     console.log("props", this.props);
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,17 +4,20 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 45 },
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
   };
+  
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter key={counter.id} value={counter.value}>
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
         ))}
       </div>
     );
