7b283f70922d2b476944d6a40cca917bc313a27e
Jam <jamayub098@gmail.com>
2024-07-14 03:12:25 +0500

    converted navBar compoenet to stateless functional compoenet

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar bg-body-tertiary">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateleff Functional Component
+const NavBar = (props) => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
 
 export default NavBar;
